81098fe8d459af470da0fa4812c34c910a6e74a0
Jessica Flores <jeflores@socasesores.com>
2019-04-08 23:58:44 -0500

    Mantenimiento de habilidades obtenidas

--- Model/Habilidad.cs
+++ Model/Habilidad.cs
@@ -1,10 +1,14 @@
+
+
 namespace Model
 {
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Habilidad")]
     public partial class Habilidad
@@ -20,5 +24,135 @@ namespace Model
         public int Dominio { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        public Habilidad Obtener(int id)
+        {
+            var Habilidad = new Habilidad();
+
+            try
+            {
+                using (var ctx = new proyectoContext())
+                {
+                    Habilidad = ctx.Habilidad.Where(x => x.id == id)
+                                                .SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return Habilidad;
+        }
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new proyectoContext())
+                {
+                    if (this.id > 0)
+                        ctx.Entry(this).State = EntityState.Modified;
+                    else
+                        ctx.Entry(this).State = EntityState.Added;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+
+            try
+            {
+                using (var ctx = new proyectoContext())
+                {
+                    this.id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="grid"></param>
+        /// <param name="tipo">Indica si es para la tabla de TrabajosRealizados=1, EstudiosPrevios=2</param>
+        /// <param name="usuario_id">Para Filtrar por el usuario que ha ingresado</param>
+        /// <returns></returns>
+        public AnexGRIDResponde Listar(AnexGRID grid, int usuario_id)
+        {
+            try
+            {
+                using (var ctx = new proyectoContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+
+                    grid.Inicializar();
+
+                    var query = ctx.Habilidad.Where(x => x.Usuario_id == usuario_id);
+
+                    //Ordenamiento
+
+                    if (grid.columna == "id")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.id)
+                                                             : query.OrderBy(x => x.id);
+                    }
+
+                    if (grid.columna == "Nombre")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Nombre)
+                                                             : query.OrderBy(x => x.Nombre);
+                    }
+
+                    if (grid.columna == "Dominio")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.Dominio)
+                                                             : query.OrderBy(x => x.Dominio);
+                    }
+
+                   
+
+                    var Habilidades = query.Skip(grid.pagina)
+                                          .Take(grid.limite)
+                                          .ToList();
+
+                    var total = query.Count();
+
+                    grid.SetData(Habilidades, total);
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            return grid.responde();
+        }
     }
 }
--- proyecto/Areas/Admin/Controllers/HabilidadesController.cs
+++ proyecto/Areas/Admin/Controllers/HabilidadesController.cs
@@ -4,16 +4,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Model;
+using Helper;
 
 namespace proyecto.Areas.Admin.Controllers
 {
     [Autenticado]
     public class HabilidadesController : Controller
     {
-        // GET: Admin/Habilidades
+        private Habilidad habilidad = new Habilidad();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult Listar(AnexGRID grid)
+        {
+            return Json(habilidad.Listar(grid, SessionHelper.GetUser()));
+        }
+
+        public ActionResult Crud(int id = 0)
+        {
+            if (id == 0)
+                habilidad.Usuario_id = SessionHelper.GetUser();
+            else
+                habilidad = habilidad.Obtener(id);
+
+            return View(habilidad);
+        }
+
+        public JsonResult Guardar(Habilidad model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/habilidades/");
+                }
+            }
+
+            return Json(rm);
+        }
+
+        public JsonResult Eliminar(int id)
+        {
+            return Json(habilidad.Eliminar(id));
         }
     }
 }
